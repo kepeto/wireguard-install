49d162b3dfdc63c8e13bbc0b4e47b9ee34c267ec
root <root@krenteg.com>
2020-04-08 05:55:45 +0700

    fix qrcode

--- new-wg-peer.sh
+++ new-wg-peer.sh
@@ -11,7 +11,7 @@ readonly PRESHARED_KEY=$(wg genpsk)
 readonly SERVER_PUBLIC_KEY=$(wg show ${INTERFACE} public-key)
 
 # Get next free peer IP (This will break after x.x.x.255)
-readonly PEER_ADDRESS=$(wg show ${INTERFACE} allowed-ips | cut -f 2 | awk -F'[./]' '{print $1"."$2"."$3"."1+$4"/"$5}' | sort -t '.' -k $
+readonly PEER_ADDRESS=$(wg show ${INTERFACE} allowed-ips | cut -f 2 | awk -F'[./]' '{print $1"."$2"."$3"."1+$4"/"$5}' | sort -t '.' -k 1,1 -k 2,2 -k 3,3 -k 4,4 -n | tail -n1)
 
 # Add peer
 wg set ${INTERFACE} peer ${PUBLIC_KEY} allowed-ips ${PEER_ADDRESS}
@@ -30,7 +30,8 @@ DNS = 1.1.1.1,8.8.8.8
 PublicKey = ${SERVER_PUBLIC_KEY}
 AllowedIPs = 0.0.0.0/0
 Endpoint = wg.krenteg.com:15000
-END_OF_CONFIG)
+END_OF_CONFIG
+)
 
-echo $conf | qrencode -t ANSIUTF8
+echo "$conf" | qrencode -t ANSIUTF8
 echo "$conf"
